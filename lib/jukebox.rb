fd28c206187b1b289b0dab88da35d99c3cf5aa16
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-13 08:38:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,14 @@
-
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
 
 def help()
   puts "I accept the following commands:"
@@ -7,3 +17,4 @@ def help()
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
+
