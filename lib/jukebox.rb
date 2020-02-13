2303216ff7823569b23bff0d9691d396eace3c8d
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-13 08:51:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,62 +10,56 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
-def help()
+def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
-  puts "- list : displays a list of songs you can play"
+  puts "- list : lets you choose a song to play"
   puts "- play : lets you choose a song to play"
   puts "- exit : exits this program"
 end
 
-def list(my_songs)
-  my_songs.each_with_index do |song, index|
-    puts "#{index+1}. #{song}"
+def list(songs)
+  songs.each_with_index do |song, i|
+    puts "#{i+1}. #{song}"
   end
 end
 
-def play(my_songs)
-  puts "Please enter a song name or number:"
-  input = gets.chomp()
-  
-  if (1..9).to_a.index(input.to_i) != nil
-    puts "Playing #{my_songs[input.to_i - 1]}"
-  elsif my_songs.index(input) != nil
-    puts "Playing #{input}"
-  else
-    puts "Invalid input, please try again"
+def play(songs)
+  puts "Please enter a song name or number"
+  input = gets.chomp
+  songs.each_with_index do |song, idx|
+    if input.to_i == (idx+1) || input == song
+      puts "Playing #{song}"
+      break
+    else
+      puts "Invalid input, please try again"
+      break
+    end 
+
   end
 end
 
-def exit_jukebox()
+def exit_jukebox
   puts "Goodbye"
 end
 
-def prompt()
-  puts "Please enter a command:"
-  gets.chomp()
-end
-
-
-def run(my_songs)
-  
-  input = prompt()
-  help()
+def run(songs)
+  help
+  input = ""
   while input != "exit"
-    if input == "list"
-      list(my_songs)
-      input = prompt()
-    elsif input == "play"
-      play(my_songs)
-      input = prompt()
-    elsif input == "help"
-      help()
-      input = prompt()
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when "play"
+      play(song)
+    when "list"
+      list(songs)
+    when "help"
+      help
+    when "exit"
+      exit_jukebox    
     else
-      puts "Invalid command"
-      help()
-      input = prompt()
+      puts "Invalid input, please try again"
     end
   end
-  exit_jukebox()
 end
